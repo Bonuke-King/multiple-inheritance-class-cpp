7c3b11535366e74f3b732a959ea5159717bb9e7e
Bonuke-King <bonuke99@gmail.com>
2022-11-27 13:05:17 +0300

    this inheritance involve multiple classes where two are base class and one is inheritance class that recieve from both the parent class

---
+++ multipleinheritance.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+//Base class
+class Myclass {
+public :
+void myFunction ()
+{
+    cout << "Some content in parent class"<<endl << endl;
+}
+
+
+};
+//another base class
+class Myotherclass {
+public :
+void myOtherFuncition()
+{
+    cout << "Some content in another class."<<endl << endl;
+}
+};
+//Derived class
+class Mychildclass :public Myclass, public Myotherclass{
+
+
+};
+int main ()
+{
+    Mychildclass obj;
+    obj.myFunction();
+    obj.myOtherFuncition();
+
+
+    return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
